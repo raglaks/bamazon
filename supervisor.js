63e047edde4e38504c6cc8d45d2d05ea1e3f8c8a
Raghava Lakshminarayana <raglaks94@gmail.com>
2018-10-29 13:18:36 -0600

    updated node module for printing table to console

--- supervisor.js
+++ supervisor.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const table = require("table");
+const table = require("console.table");
 
 const connection = mysql.createConnection({
 
